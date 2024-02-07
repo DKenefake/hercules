049a56d9ddbd45b441a1b2273d1a4a0e97674bb5
Dustin Kenefake <Dustin.Kenefake@tamu.edi>
2024-02-07 16:46:05 -0600

    removed duplication in branching rules

--- src/branchbound_utils.rs
+++ src/branchbound_utils.rs
@@ -90,29 +90,14 @@ pub fn random(solver: &BBSolver, node: &QuboBBNode) -> usize {
 
 /// Branches on the variable that has an estimated worst result, pushing up the lower bound as fast as possible
 pub fn worst_approximation(solver: &BBSolver, node: &QuboBBNode) -> usize {
-    let mut zero_buffer = node.solution.clone();
-    let mut one_buffer = node.solution.clone();
 
-    // fix all the nonfixed variables
-    for i in 0..solver.qubo.num_x() {
-        if node.fixed_variables.contains_key(&i) {
-            zero_buffer[i] = *node.fixed_variables.get(&i).unwrap();
-            one_buffer[i] = *node.fixed_variables.get(&i).unwrap();
-        } else {
-            zero_buffer[i] = 0.0;
-            one_buffer[i] = 1.0;
-        }
-    }
-
-    // find the objective change when flipping each variable to either 0 or 1
-    let (_, flips_zero) = local_search_utils::one_flip_objective(&solver.qubo, &zero_buffer);
-    let (_, flips_one) = local_search_utils::one_flip_objective(&solver.qubo, &one_buffer);
+    let (zero_flip, one_flip) = compute_strong_branch(solver, node);
 
     // tracking variables for the worst approximation
     let mut worst_approximation = f64::NEG_INFINITY;
     let mut index_worst_approximation = 0;
 
-    // scan thru the variables and find the worst gain
+    // scan through the variables and find the worst gain
     for i in 0..solver.qubo.num_x() {
         // if it is a fixed node, then skip it
         if node.fixed_variables.contains_key(&i) {
@@ -120,7 +105,7 @@ pub fn worst_approximation(solver: &BBSolver, node: &QuboBBNode) -> usize {
         }
 
         // find the minimum of the two objective changes
-        let min_obj_gain = flips_zero[i].min(flips_one[i]);
+        let min_obj_gain = zero_flip[i].min(one_flip[i]);
 
         // if it is the highest growing variable, then update the tracking variables
         if min_obj_gain > worst_approximation {
@@ -134,27 +119,13 @@ pub fn worst_approximation(solver: &BBSolver, node: &QuboBBNode) -> usize {
 
 /// Branches on the variable that has an estimated best result,keeping the lower bound as low as possible
 pub fn best_approximation(solver: &BBSolver, node: &QuboBBNode) -> usize {
-    let mut zero_buffer = node.solution.clone();
-    let mut one_buffer = node.solution.clone();
 
-    // fix all the nonfixed variables
-    for i in 0..solver.qubo.num_x() {
-        if node.fixed_variables.contains_key(&i) {
-            zero_buffer[i] = *node.fixed_variables.get(&i).unwrap();
-            one_buffer[i] = *node.fixed_variables.get(&i).unwrap();
-        } else {
-            zero_buffer[i] = 0.0;
-            one_buffer[i] = 1.0;
-        }
-    }
-
-    // find the objective change when flipping each variable to either 0 or 1
-    let (_, flips_zero) = local_search_utils::one_flip_objective(&solver.qubo, &zero_buffer);
-    let (_, flips_one) = local_search_utils::one_flip_objective(&solver.qubo, &one_buffer);
+    let (zero_flip, one_flip) = compute_strong_branch(solver, node);
 
     // tracking variables for the worst approximation
     let mut worst_approximation = f64::INFINITY;
-    let mut index_worst_approximation = 0;
+    let mut index_best_approximation = 0;
+
 
     // scan through the variables and find the worst gain
     for i in 0..solver.qubo.num_x() {
@@ -164,17 +135,56 @@ pub fn best_approximation(solver: &BBSolver, node: &QuboBBNode) -> usize {
         }
 
         // find the minimum of the two objective changes
-        let max_obj_gain = flips_zero[i].max(flips_one[i]);
+        let max_obj_gain = zero_flip[i].max(one_flip[i]);
 
         // if it is the highest growing variable, then update the tracking variables
         if max_obj_gain <= worst_approximation {
             worst_approximation = max_obj_gain;
-            index_worst_approximation = i;
+            index_best_approximation = i;
         }
     }
 
-    index_worst_approximation
+    index_best_approximation
 }
+
+pub fn compute_strong_branch(solver: &BBSolver, node: &QuboBBNode) -> (Array1<f64>, Array1<f64>){
+
+    let mut base_solution = Array1::zeros(solver.qubo.num_x());
+    let mut delta_zero = Array1::zeros(solver.qubo.num_x());
+    let mut delta_one = Array1::zeros(solver.qubo.num_x());
+
+    for i in 0..solver.qubo.num_x(){
+        // fill in the current vector
+        if node.fixed_variables.contains_key(&i){
+            base_solution[i] = *node.fixed_variables.get(&i).unwrap();
+        }else{
+            base_solution[i] = node.solution[i];
+        }
+
+        // compute the delta values for the zero and one flips
+        delta_zero[i] = -base_solution[i];
+        delta_one[i] = 1.0 - base_solution[i];
+    }
+
+    // build the intermediate vectors
+    let q_jj = solver.qubo.q.diag().to_dense();
+    let q_x = &solver.qubo.q * &base_solution;
+    let x_q = &solver.qubo.q.transpose_view() * &base_solution;
+
+
+    // build the result vectors
+    let mut zero_result = Array1::zeros(solver.qubo.num_x());
+    let mut one_result = Array1::zeros(solver.qubo.num_x());
+
+    // compute the deltas in the objective compared to the current solution
+    for i in 0..solver.qubo.num_x(){
+        zero_result[i] = 0.5*(delta_zero[i]*delta_zero[i]*q_jj[i] + delta_zero[i]*(x_q[i] + q_x[i] + 2.0*solver.qubo.c[i]));
+        one_result[i] = 0.5*(delta_one[i]*delta_one[i]*q_jj[i] + delta_one[i]*(x_q[i] + q_x[i] + 2.0*solver.qubo.c[i]));
+    }
+
+    (zero_result, one_result)
+}
+
 
 /// Wrapper to help convert the QUBO to the format required by Clarabel.rs
 pub struct ClarabelWrapper {
