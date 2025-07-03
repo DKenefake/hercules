8a4ad1e1b5c9e0343b2a0cb7bb76ac69c24e283a
Dustin Kenefake <Dustin.Kenefake@tamu.edi>
2025-07-03 11:47:02 -0400

    remove unnessisary repeated allocs in enumerate solve

--- src/subproblemsolvers/enumerate_qubo.rs
+++ src/subproblemsolvers/enumerate_qubo.rs
@@ -6,10 +6,10 @@ pub fn enumerate_solve(qubo: &Qubo) -> (f64, Array1<usize>) {
     let num_vars = qubo.num_x();
     let mut best_obj = f64::INFINITY;
     let mut best_solution = Array1::<usize>::zeros(num_vars);
+    let mut solution = Array1::<usize>::zeros(num_vars);
 
     for i in 0..(1 << num_vars) {
 
-        let mut solution = Array1::<usize>::zeros(num_vars);
         for j in 0..num_vars {
             solution[j] = (i >> j) & 1;
         }
